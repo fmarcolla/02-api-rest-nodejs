2317aeec269fc9b365af7bf4def741ae820da697
Marcolla <felipem.marcolla@gmail.com>
2023-09-10 18:26:58 -0300

    Change env config

--- src/env/index.ts
+++ src/env/index.ts
@@ -14,7 +14,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
 })
 
-export const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('Invalid environment variables!', _env.error.format())
